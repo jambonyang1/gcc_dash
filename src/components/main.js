4ec130dc9af474b91c407ab9f0332a65cf1d8bfb
jambonyang1 <1106wjdgus@gmail.com>
2024-05-12 10:00:46 +0900

    메인화면 하드코딩

--- src/components/main.js
+++ src/components/main.js
@@ -1,17 +1,128 @@
 import React from "react";
 import styled from "styled-components";
-import data from "./work.json";
-import Work from "./work";
-
+import { Image } from "antd";
+import { Card } from "antd";
+import work_coding from "./work_coding.jpeg";
+import work_dog from "./work_dog.png";
+import work_kid from "./work_kid.jpg";
+import work_music from "./work_music.jpeg";
+import work_english from "./work_english.jpeg";
+import work_dosirak from "./work_dosirak.jpeg";
+const { Meta } = Card;
 const Main = () => {
   return (
     <StyledWrapper>
-      {data.data.map((event, index) => (
-        <Work key={index} event={event} />
-      ))}
+      <Card
+        cover={<Image alt="example" src={work_coding} />}
+        actions={[
+          <div className="button-orange">신청하기</div>,
+          <div className="button">자세히</div>,
+        ]}
+      >
+        <Meta
+          title="코딩 멘토링 참가자 모집"
+          description={
+            <div className="box">
+              <p className="item">장소: 경기도 성남시 분당구 정자동</p>
+              <p className="item">날짜: 5월 28일 14:00</p>
+            </div>
+          }
+        />
+      </Card>
+      <Card
+        cover={<Image alt="example" src={work_dog} />}
+        actions={[
+          <div className="button-orange">신청하기</div>,
+          <div className="button">자세히</div>,
+        ]}
+      >
+        <Meta
+          title="유기견 산책 봉사"
+          description={
+            <div className="box">
+              <p className="item">서울시 성동구 광나루동</p>
+              <p className="item">날짜: 5월 23일 12:00</p>
+            </div>
+          }
+        />
+      </Card>
+      <Card
+        cover={<Image alt="example" src={work_english} />}
+        actions={[
+          <div className="button-orange">신청하기</div>,
+          <div className="button">자세히</div>,
+        ]}
+      >
+        <Meta
+          title="영어 회화 수업 선생님 모집"
+          description={
+            <div className="box">
+              <p className="item">경기도 고양시 일산동</p>
+              <p className="item">날짜: 5월 30일 18:00</p>
+            </div>
+          }
+        />
+      </Card>
+      <Card
+        cover={<Image alt="example" src={work_kid} />}
+        actions={[
+          <div className="button-orange">신청하기</div>,
+          <div className="button">자세히</div>,
+        ]}
+      >
+        <Meta
+          title="하늘보육원 음악강의"
+          description={
+            <div className="box">
+              <p className="item">서울시 송파구 방이동</p>
+              <p className="item">날짜: 5월 23일 16:00</p>
+            </div>
+          }
+        />
+      </Card>
+      <Card
+        cover={<Image alt="example" src={work_dosirak} />}
+        actions={[
+          <div className="button-orange">신청하기</div>,
+          <div className="button">자세히</div>,
+        ]}
+      >
+        <Meta
+          title="도시락 나눔 봉사활동 참여자 모집"
+          description={
+            <div className="box">
+              <p className="item">서울시 강남구 역삼동</p>
+              <p className="item">날짜: 5월 29일 11:00</p>
+            </div>
+          }
+        />
+      </Card>
     </StyledWrapper>
   );
 };
 export default Main;
 
-const StyledWrapper = styled.div``;
+const StyledWrapper = styled.div`
+  padding: 20px;
+  gap: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+
+  .button-orange {
+    color: #ff7a00;
+    cursor: pointer;
+  }
+
+  .button {
+    color: black;
+    cursor: pointer;
+  }
+
+  .box {
+    padding: 0px;
+    .item {
+      padding: 0px;
+    }
+  }
+`;
--- src/components/work.js
+++ src/components/work.js
@@ -6,15 +6,6 @@ const Work = (props) => {
   const { event } = props;
   return (
     <StyledWrapper>
-      {/* <div className="box">
-        <h3>{event.title}</h3>
-        <p>장소: {event.place}</p>
-        <p>날짜: {event.date}</p>
-        <p>소요 시간: {event.duration}</p>
-        <p>키워드: {event.keyword.join(", ")}</p>
-        <p>상세 정보: {event.info}</p>
-        <p>요청 여부: {event.request ? "요청 중" : "요청 중 아님"}</p>
-      </div> */}
       <Card
         cover={
           <img
